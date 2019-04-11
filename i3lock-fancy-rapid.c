6b448e54b37b9fa0e93875c0e64dec1b16b0b066
Martin Dørum <martid0311@gmail.com>
2019-04-11 20:11:11 +0200

    faster

--- i3lock-fancy-rapid.c
+++ i3lock-fancy-rapid.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 #include <malloc.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <omp.h>
-#include "lodepng/lodepng.h"
 
-void box_blur_h(unsigned char *dest, unsigned char *src, int height, int width,
+void box_blur_h(uint32_t *dest, uint32_t *src, int height, int width,
                 int radius)
 {
     double coeff = 1.0 / (radius * 2 + 1);
@@ -20,28 +21,29 @@ void box_blur_h(unsigned char *dest, unsigned char *src, int height, int width,
         double b_acc = 0.0;
         for (int j = -radius; j < width; ++j) {
             if (j - radius - 1 >= 0) {
-                int index = (iwidth + j - radius - 1) * 3;
-                r_acc -= coeff * src[index];
-                g_acc -= coeff * src[index + 1];
-                b_acc -= coeff * src[index + 2];
+                int index = iwidth + j - radius - 1;
+                r_acc -= coeff * ((src[index] & 0xff0000) >> 16);
+                g_acc -= coeff * ((src[index] & 0x00ff00) >> 8);
+                b_acc -= coeff * ((src[index] & 0x0000ff));
             }
             if (j + radius < width) {
-                int index = (iwidth + j + radius) * 3;
-                r_acc += coeff * src[index];
-                g_acc += coeff * src[index + 1];
-                b_acc += coeff * src[index + 2];
+                int index = iwidth + j + radius;
+                r_acc += coeff * ((src[index] & 0xff0000) >> 16);
+                g_acc += coeff * ((src[index] & 0x00ff00) >> 8);
+                b_acc += coeff * ((src[index] & 0x0000ff));
             }
             if (j < 0)
                 continue;
-            int index = (iwidth + j) * 3;
-            dest[index] = r_acc + 0.5;
-            dest[index + 1] = g_acc + 0.5;
-            dest[index + 2] = b_acc + 0.5;
+            int index = iwidth + j;
+            dest[index] = 0 |
+                (((uint32_t)(r_acc + 0.5) & 0xff) << 16) |
+                (((uint32_t)(g_acc + 0.5) & 0xff) << 8) |
+                (((uint32_t)(b_acc + 0.5) & 0xff));
         }
     }
 }
 
-void box_blur_v(unsigned char *dest, unsigned char *src, int height, int width,
+void box_blur_v(uint32_t *dest, uint32_t *src, int height, int width,
                 int radius)
 {
     double coeff = 1.0 / (radius * 2 + 1);
@@ -52,45 +54,54 @@ void box_blur_v(unsigned char *dest, unsigned char *src, int height, int width,
         double b_acc = 0.0;
         for (int i = -radius; i < height; ++i) {
             if (i - radius - 1 >= 0) {
-                int index = ((i - radius - 1) * width + j) * 3;
-                r_acc -= coeff * src[index];
-                g_acc -= coeff * src[index + 1];
-                b_acc -= coeff * src[index + 2];
+                int index = (i - radius - 1) * width + j;
+                r_acc -= coeff * ((src[index] & 0xff0000) >> 16);
+                g_acc -= coeff * ((src[index] & 0x00ff00) >> 8);
+                b_acc -= coeff * ((src[index] & 0x0000ff));
             }
             if (i + radius < height) {
-                int index = ((i + radius) * width + j) * 3;
-                r_acc += coeff * src[index];
-                g_acc += coeff * src[index + 1];
-                b_acc += coeff * src[index + 2];
+                int index = (i + radius) * width + j;
+                r_acc += coeff * ((src[index] & 0xff0000) >> 16);
+                g_acc += coeff * ((src[index] & 0x00ff00) >> 8);
+                b_acc += coeff * ((src[index] & 0x0000ff));
             }
             if (i < 0)
                 continue;
-            int index = (i * width + j) * 3;
-            dest[index] = r_acc + 0.5;
-            dest[index + 1] = g_acc + 0.5;
-            dest[index + 2] = b_acc + 0.5;
+            int index = i * width + j;
+            dest[index] = 0 |
+                (((uint32_t)(r_acc + 0.5) & 0xff) << 16) |
+                (((uint32_t)(g_acc + 0.5) & 0xff) << 8) |
+                (((uint32_t)(b_acc + 0.5) & 0xff));
         }
     }
 }
 
-void box_blur_once(unsigned char *dest, unsigned char *src, int height,
-                   int width, int radius)
+void box_blur_once(uint32_t *dest, uint32_t *src, uint32_t *scratch,
+        int height, int width, int radius)
 {
-    unsigned char *tmp = malloc(height * width * 3);
-    box_blur_h(tmp, src, height, width, radius);
-    box_blur_v(dest, tmp, height, width, radius);
-    free(tmp);
+    box_blur_h(scratch, src, height, width, radius);
+    box_blur_v(dest, scratch, height, width, radius);
 }
 
-void box_blur(unsigned char *dest, unsigned char *src, int height, int width,
+void box_blur(uint32_t *dest, uint32_t *src, int height, int width,
               int radius, int times)
 {
-    box_blur_once(dest, src, height, width, radius);
-    for (int i = 0; i < times - 1; ++i) {
-        memcpy(src, dest, height * width * 3);
-        box_blur_once(dest, src, height, width, radius);
-    }
+    uint32_t *origdest = dest;
 
+    uint32_t *scratch = malloc(width * height * sizeof(*scratch));
+    box_blur_once(dest, src, scratch, height, width, radius);
+    for (int i = 0; i < times - 1; ++i) {
+        uint32_t *tmp = src;
+        src = dest;
+        dest = tmp;
+        box_blur_once(dest, src, scratch, height, width, radius);
+    }
+    free(scratch);
+
+    // We're flipping between using dest and src;
+    // if the last buffer we used was src, copy that over to dest.
+    if (dest != origdest)
+        memcpy(origdest, dest, width * height * sizeof(*dest));
 }
 
 int main(int argc, char *argv[])
@@ -105,52 +116,44 @@ int main(int argc, char *argv[])
     XGetWindowAttributes(display, root, &gwa);
     int height = gwa.height;
     int width = gwa.width;
-    unsigned char *preblur = malloc(height * width * 3);
+    uint32_t *preblur = malloc(height * width * sizeof(*preblur));
     XImage *image = XGetImage(display, root, 0, 0, width, height, AllPlanes,
                               ZPixmap);
     for (int i = 0; i < height; ++i) {
         int iwidth = i * width;
         for (int j = 0; j < width; ++j) {
-            int index = (iwidth + j) * 3;
+            int index = iwidth + j;
             unsigned long pixel = XGetPixel(image, j, i);
-            preblur[index] = (pixel & image->red_mask) >> 16;
-            preblur[index + 1] = (pixel & image->green_mask) >> 8;
-            preblur[index + 2] = pixel & image->blue_mask;
+            preblur[index] = pixel & 0x00ffffff;
         }
     }
     XDestroyImage(image);
     XDestroyWindow(display, root);
     XCloseDisplay(display);
-    unsigned char *postblur = malloc(height * width * 3);
+
+    uint32_t *postblur = malloc(height * width * sizeof(*postblur));
     box_blur(postblur, preblur, height, width, atoi(argv[1]), atoi(argv[2]));
     free(preblur);
-    LodePNGState state;
-    lodepng_state_init(&state);
-    state.info_raw.colortype = LCT_RGB;
-    state.encoder.zlibsettings.btype = 0;
-    unsigned char *data;
-    size_t data_len;
-    lodepng_encode(&data, &data_len, postblur, width, height, &state);
-    free(postblur);
-    lodepng_state_cleanup(&state);
-    char filename[] = "/tmp/tmp.XXXXXX.png";
-    int fd = mkstemps(filename, 4);
-    write(fd, data, data_len);
-    free(data);
-    close(fd);
+
+    int fds[2];
+    pipe(fds);
     if (fork()) {
+        write(fds[1], postblur, height * width * sizeof(*postblur));
         int status;
         wait(&status);
-        remove(filename);
         exit(WEXITSTATUS(status));
     } else {
-        char *new_argv[argc + 1];
+        dup2(fds[0], STDIN_FILENO);
+        char geometry[32];
+        snprintf(geometry, sizeof(geometry), "%ix%i", width, height);
+
+        char *new_argv[6];
         new_argv[0] = "i3lock";
         new_argv[1] = "-i";
-        new_argv[2] = filename;
-        for (int i = 3; i < argc; ++i)
-            new_argv[i] = argv[i];
-        new_argv[argc] = NULL;
+        new_argv[2] = "/dev/stdin";
+        new_argv[3] = "-r";
+        new_argv[4] = geometry;
+        new_argv[5] = NULL;
         execvp(new_argv[0], new_argv);
         exit(EXIT_FAILURE);
     }
